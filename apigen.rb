db262980f16b796b35b460872e9bd4a6baa8d104
luca3104 <s1210006@u-aizu.ac.jp>
2017-02-22 18:45:33 +0900

    0.0.3

--- apigen.rb
+++ apigen.rb
@@ -6,7 +6,7 @@ class Apigen < Formula
   desc ""
   homepage ""
   url "https://github.com/luca3104/apigen/archive/v0.0.3.tar.gz"
-  sha256 "0c2b0b2594c6255d0b804a0d77a11a162cbac4957313e9b7747d0796207559be"
+  sha256 "1c9eb2621d1a505103a1d6ca3202708b911cdeadaf24a5160550faff0debf455"
 
   # depends_on "cmake" => :build
 
